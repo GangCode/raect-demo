d9e74c6a8496bbf84de37c07637ac9da5acc4063
紫珺 <@N551>
2016-09-11 18:37:44 +0800

    Merge origin/master
    Conflicts:
    	public_html/css/style.css
    	public_html/index.html

--- public/js/apps/demo1.js
+++ public/js/apps/demo1.js
@@ -1,19 +1,40 @@
+var items = [
+    { id : 0, name : '主页'},
+    { id : 1, name : '产品'},
+    { id : 2, name : '关于我们'}
+];
+
+/**
+ * react组件形式渲染html
+ */
 
 var NavBar = React.createClass({
     displayName: 'NavBar',
     render : function () {
         return (
-            <ul><li>主页</li><li>产品</li><li>关于我们</li></ul>
+            <ul>
+                {
+                    items.map(function (item) {
+                        return <li key={item.id}>{item.name}</li>
+                    })
+                }
+            </ul>
         );
     }
 });
 ReactDOM.render(React.createElement(NavBar, null), document.getElementById('components'));
 
+/**
+ * react标签形式渲染html
+ */
+items.push({ id : 3, name : '下载'});
 var navBar = (
     <ul>
-        <li>主页</li>
-        <li>产品</li>
-        <li>关于我们</li>
+        {
+            items.map(function (item) {
+                return <li key={item.id}>{item.name}</li>
+            })
+        }
     </ul>
 );
 ReactDOM.render(navBar, document.getElementById('html_tag'));
