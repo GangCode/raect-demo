5af01376a606f6c1c3f88f49ce6ce5a7bf2a2268
yinzhigang <yinzhigang@yundun.com>
2016-09-13 10:02:25 +0800

    添加文件

--- public/js/apps/demo3.js
+++ public/js/apps/demo3.js
@@ -1,39 +1,50 @@
 
-var CheckBoxComment = React.createClass({
-    render: function() {
+var InputComment = React.createClass({
+    render : function () {
         return (
-            <label>{this.props.labelText}:<input type='checkbox' name='switch' value={this.props.radioVal} /></label>
+            <label>{this.props.labelText}:
+                <input type={this.props.inputType} name={this.props.name} onChange={this.handleChange} placeholder={this.props.tips}/>
+            </label>
+        );
+    },
+    handleChange : function (event) {
+        console.log(event.target.value)
+        this.setState({value: event.target.value});
+    }
+});
+
+var ButtonComment = React.createClass({
+    render : function(){
+        return (
+            <span className='btn'>
+                <button type={this.props.btnType}>{this.props.btnText}</button>
+            </span>
         );
     }
 });
 
-class ShowComment extends React.Component{
-    constructor() {
-        super();
-        this.state = {
-            checked: false
-        };
-        this.handleClick = this.handleClick.bind(this);
-    } 
-    
-    handleClick(){
-        this.setState({ checked : !this.state.checked });
-        console.log("changed")
-    }
-    
-    render(){
-        const text = this.state.checked ? 'checked' : 'haven\'t checked';
+var LoginComment = React.createClass({
+    render : function(){
         return (
-            <div>
-                <CheckBoxComment labelText='关' radioVal='0' onClick={this.handleClick}/>
-                <CheckBoxComment labelText='开' radioVal='1' onClick={this.handleClick}/>
-                <div id='show_select'>{text}</div>
-            </div>
+            <form method='get' action=''>
+                <div className='login'>
+                    <div>
+                        <InputComment inputType='text' name='username' labelText='用户名' tips='请输入用户名'/>
+                    </div>
+                    <div>
+                        <InputComment inputType='password' name='password' labelText='密&nbsp;码' tips='请输入密码'/>
+                    </div>
+                    <div>
+                        <ButtonComment btnType='submit' btnText='登录'/>
+                        <ButtonComment btnType='reset' btnText='重置'/>
+                    </div>
+                </div>
+            </form>
         );
     }
-}
+});
 
-ReactDOM.render(<ShowComment/>,document.getElementById('text'));
+ReactDOM.render(<LoginComment />,document.getElementById('text'));
 
 
 
